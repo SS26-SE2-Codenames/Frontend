5254051c2b6fea2a248da3558cdf5d785912d302
ad-devel <adgaming.dev@gmail.com>
2026-04-01 17:41:33 +0200

    Basic Button functionality

--- app/src/main/java/com/codenames/codenames_frontend/MainActivity.kt
+++ app/src/main/java/com/codenames/codenames_frontend/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.codenames.codenames_frontend.ui.buttons.AppButton
+import com.codenames.codenames_frontend.ui.buttons.AppButtonType
 import com.codenames.codenames_frontend.ui.theme.CodenamesTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +23,38 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CodenamesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ButtonTestScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun ButtonTestScreen() {
+    var message by remember { mutableStateOf("Noch nichts geklickt") }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CodenamesTheme {
-        Greeting("Android")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(message)
+
+        AppButton(
+            text = "Spiel erstellen",
+            onClick = {
+                message = "Primary Button wurde geklickt"
+            }
+        )
+
+        AppButton(
+            text = "Beitreten",
+            onClick = {
+                message = "Secondary Button wurde geklickt"
+            },
+            type = AppButtonType.SECONDARY
+        )
     }
 }
