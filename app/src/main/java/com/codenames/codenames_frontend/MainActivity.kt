314c56b6668b0a36d2e88c12bb2c8a3c814953dd
5eli <prettnerseli@gamil.com>
2026-04-02 15:40:39 +0200

    added different screens and navigation between them through buttons

--- app/src/main/java/com/codenames/codenames_frontend/MainActivity.kt
+++ app/src/main/java/com/codenames/codenames_frontend/MainActivity.kt
@@ -4,17 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.codenames.codenames_frontend.ui.buttons.AppButton
-import com.codenames.codenames_frontend.ui.buttons.AppButtonType
+import androidx.compose.runtime.Composable
+import com.codenames.codenames_frontend.ui.navigation.NavGraph
 import com.codenames.codenames_frontend.ui.theme.CodenamesTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,38 +14,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CodenamesTheme {
-                ButtonTestScreen()
+                AppContent()
             }
         }
     }
 }
 
 @Composable
-fun ButtonTestScreen() {
-    var message by remember { mutableStateOf("Noch nichts geklickt") }
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(24.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(message)
-
-        AppButton(
-            text = "Spiel erstellen",
-            onClick = {
-                message = "Primary Button wurde geklickt"
-            }
-        )
-
-        AppButton(
-            text = "Beitreten",
-            onClick = {
-                message = "Secondary Button wurde geklickt"
-            },
-            type = AppButtonType.SECONDARY
-        )
-    }
+fun AppContent() {
+    NavGraph()
 }
