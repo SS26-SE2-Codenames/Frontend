8cacc0ff052f2c7e957109f0badcc36b7375b2f1
ad-devel <adgaming.dev@gmail.com>
2026-04-06 12:07:35 +0200

    extended Button customization

--- app/src/main/java/com/codenames/codenames_frontend/ui/screens/StartScreen.kt
+++ app/src/main/java/com/codenames/codenames_frontend/ui/screens/StartScreen.kt
@@ -1,17 +1,19 @@
 package com.codenames.codenames_frontend.ui.screens
-import com.codenames.codenames_frontend.ui.buttons.AppButton
-import androidx.compose.runtime.Composable
+
 import androidx.compose.foundation.layout.*
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.codenames.codenames_frontend.ui.buttons.AppButton
 import com.codenames.codenames_frontend.ui.navigation.Screen
-
 
 @Composable
 fun StartScreen(navController: NavHostController) {
     ForceLandscape()
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -23,15 +25,23 @@ fun StartScreen(navController: NavHostController) {
             text = "Create Lobby",
             onClick = {
                 navController.navigate(Screen.Lobby.route)
-            }
+            },
+            containerColor = Color(0xFF2E7D32),
+            contentColor = Color.White,
+            modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .padding(bottom = 12.dp)
         )
+
         AppButton(
             text = "Join Lobby",
             onClick = {
                 navController.navigate(Screen.JoinLobby.route)
-            }
+            },
+            containerColor = Color(0xFF1565C0),
+            contentColor = Color.White,
+            modifier = Modifier
+                .fillMaxWidth(0.5f)
         )
     }
 }
-
-
