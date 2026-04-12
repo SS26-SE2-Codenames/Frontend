385a2d30695d6f2b7c9b09979969b99b9e32be66
5eli <prettnerseli@gamil.com>
2026-04-12 17:47:09 +0200

    updated UI manager functionality

--- app/src/main/java/com/codenames/codenames_frontend/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/codenames/codenames_frontend/ui/navigation/NavGraph.kt
@@ -1,41 +1,100 @@
 package com.codenames.codenames_frontend.ui.navigation
 
-import androidx.compose.runtime.Composable
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.codenames.codenames_frontend.ui.screens.GameSettingsScreen
-import com.codenames.codenames_frontend.ui.screens.GameboardScreen
-import com.codenames.codenames_frontend.ui.screens.JoinlobbyScreen
-import com.codenames.codenames_frontend.ui.screens.LobbyScreen
-import com.codenames.codenames_frontend.ui.screens.SettingsScreen
-import com.codenames.codenames_frontend.ui.screens.StartScreen
+import androidx.compose.runtime.*
+import androidx.navigation.NavType
+import androidx.navigation.compose.*
+import androidx.navigation.navArgument
+
+import com.codenames.codenames_frontend.ui.roles.PlayerRole
+import com.codenames.codenames_frontend.ui.screens.*
 
 @Composable
 fun NavGraph() {
+
     val navController = rememberNavController()
 
     NavHost(
         navController = navController,
         startDestination = Screen.Start.route
     ) {
+
         composable(Screen.Start.route) {
             StartScreen(navController)
         }
+
         composable(Screen.Lobby.route) {
             LobbyScreen(navController)
         }
+
         composable(Screen.JoinLobby.route) {
             JoinlobbyScreen()
         }
-        composable(Screen.Gameboard.route) {
-            GameboardScreen()
+
+        // ---------------- GAME SCREEN ----------------
+        composable(
+            route = "${Screen.Gameboard.route}/{role}",
+            arguments = listOf(navArgument("role") { type = NavType.StringType })
+        ) { backStackEntry ->
+
+            val roleString =
+                backStackEntry.arguments?.getString("role") ?: PlayerRole.NONE.name
+
+            val passedRole = try {
+                PlayerRole.valueOf(roleString)
+            } catch (e: IllegalArgumentException) {
+                PlayerRole.NONE
+            }
+
+            GameScreenWrapper(userRole = passedRole)
         }
+
         composable(Screen.GameSettings.route) {
             GameSettingsScreen()
         }
+
         composable(Screen.Settings.route) {
             SettingsScreen()
         }
+
+        composable("game_test") {
+            GameTestScreen()
+        }
     }
+}
+
+@Composable
+fun GameScreenWrapper(userRole: PlayerRole) {
+
+    var currentHint by remember { mutableStateOf("Waiting for hint...") }
+
+    val cards = remember {
+        mutableStateListOf(
+            *List(25) {
+                GameCard(
+                    word = "Word ${it + 1}",
+                    type = when (it) {
+                        0 -> CardType.ASSASSIN
+                        in 1..8 -> CardType.BLUE
+                        in 9..15 -> CardType.RED
+                        else -> CardType.NEUTRAL
+                    }
+                )
+            }.toTypedArray()
+        )
+    }
+
+    fun revealCard(index: Int) {
+        val card = cards[index]
+        cards[index] = card.copy(revealed = true)
+    }
+
+    GameboardScreen(
+        userRole = userRole,
+        currentHint = currentHint,
+        onHintChange = { currentHint = it },
+        cards = cards,
+        onReveal = { index ->
+            revealCard(index)
+        }
+    )
 }
---
+++ app/src/main/java/com/codenames/codenames_frontend/ui/roles/PlayerRoles.kt
@@ -0,0 +1,9 @@
+package com.codenames.codenames_frontend.ui.roles
+
+enum class PlayerRole {
+    NONE,
+    BLUE_OPERATIVE,
+    BLUE_SPYMASTER,
+    RED_OPERATIVE,
+    RED_SPYMASTER
+}
