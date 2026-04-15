78f803a1c7cd88a8be53a98c93fb1a8600c729c4
the-only-queen-anna <annpschernig@edu.aau.at>
2026-04-15 11:17:06 +0200

    added default values for Game Message

--- app/src/main/java/com/codenames/codenames_frontend/network/dto/GameMessage.kt
+++ app/src/main/java/com/codenames/codenames_frontend/network/dto/GameMessage.kt
@@ -3,4 +3,11 @@ package com.codenames.codenames_frontend.network.dto
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class GameMessage(val winner: String, val currentTurn: String, val currentRedFound: Int, val currentBlueFound: Int, val currentClue: String, val remainingGuesses: Int, val cardList: List<CardDto>)
+data class GameMessage(
+    val winner: String = "",
+    val currentTurn: String = "",
+    val currentRedFound: Int = 0,
+    val currentBlueFound: Int = 0,
+    val currentClue: String = "",
+    val remainingGuesses: Int = 0,
+    val cardList: List<CardDto> = emptyList())
