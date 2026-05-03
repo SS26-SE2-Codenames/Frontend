e815cafba9455dae9afc521a097f9e7887eb742f
Anna Pschernig <annpschernig@edu.aau.at>
2026-05-03 17:30:01 +0200

    refactoring of lobby screen

--- app/src/main/java/com/codenames/frontend/ui/screens/LobbyScreen.kt
+++ app/src/main/java/com/codenames/frontend/ui/screens/LobbyScreen.kt
@@ -23,8 +23,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
-import com.codenames.frontend.data.model.enums.Role
 import com.codenames.frontend.data.model.enums.Team
 import com.codenames.frontend.ui.buttons.AppButton
 import com.codenames.frontend.ui.buttons.AppButtonStyle
@@ -67,8 +67,8 @@ val greenGradient =
             ),
     )
 
-const val join_team : String = "JOIN TEAM"
-const val team_joined : String = "👤 1 joined"
+private const val JOIN_TEAM : String = "JOIN TEAM"
+private const val TEAM_JOINED : String = "👤 1 joined"
 
 @Suppress("ktlint:standard:function-naming")
 @Composable
@@ -84,237 +84,188 @@ fun LobbyScreen(navController: NavHostController) {
         horizontalArrangement = Arrangement.SpaceEvenly,
         verticalAlignment = Alignment.CenterVertically,
     ) {
-        Column(
+
+        TeamColumn(
             modifier = Modifier.weight(1f),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
+            color = Team.BLUE,
+            gradient = blueGradient,
+            textColor = Color(0xFF42A5F5),
+            title = "BLUE TEAM",
+            currentRole = currentRole,
+            onRoleSelect = { currentRole = it }
+        )
+
+        GameSettingsColumn(
+            currentRole = currentRole,
+            navController = navController
+        )
+
+        TeamColumn(
+            modifier = Modifier.weight(1f),
+            color = Team.RED,
+            gradient = redGradient,
+            textColor = Color(0xFFDE8468),
+            title = "RED TEAM",
+            currentRole = currentRole,
+            onRoleSelect = { currentRole = it }
+        )
+    }
+}
+
+@Composable
+fun TeamColumn(
+    modifier: Modifier,
+    color: Team,
+    gradient: Brush,
+    textColor: Color,
+    title: String,
+    currentRole: PlayerRoles,
+    onRoleSelect: (PlayerRoles) -> Unit
+){
+    val align = if (color == Team.RED) Alignment.End else Alignment.Start
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        val cardModifier = Modifier
+            .align(align)
+            .width(200.dp)
+            .height(150.dp)
+            .fillMaxWidth(0.5f)
+            .padding(start = 6.dp, bottom = 12.dp)
+            .background(gradient, RoundedCornerShape(12.dp))
+            .padding(12.dp)
+
+        Text(
+            text = title,
+            color = textColor,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier =
+                Modifier
+                    .align(align)
+                    .padding(start = 6.dp)
+                    .padding(end = 6.dp)
+                    .padding(bottom = 6.dp),
+        )
+
+        RoleCard(
+            role = if(color == Team.RED) PlayerRoles.RED_OPERATIVE else PlayerRoles.BLUE_OPERATIVE,
+            currentRole = currentRole,
+            onRoleSelect = onRoleSelect,
+            modifier = cardModifier,
+            title = "OPERATIVES"
+        )
+
+        RoleCard(
+            role = if(color == Team.RED) PlayerRoles.RED_SPYMASTER else PlayerRoles.BLUE_SPYMASTER,
+            currentRole = currentRole,
+            onRoleSelect = onRoleSelect,
+            modifier = cardModifier,
+            title = "SPYMASTERS"
+        )
+    }
+}
+
+@Composable
+fun RoleCard(
+    role: PlayerRoles,
+    currentRole: PlayerRoles,
+    onRoleSelect: (PlayerRoles) -> Unit,
+    modifier: Modifier,
+    title: String
+){
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Text(title, color = Color.White, fontWeight = FontWeight.Bold)
+
+        if (currentRole == role) {
             Text(
-                text = "BLUE TEAM",
-                color = Color(0xFF42A5F5),
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold,
-                modifier =
-                    Modifier
-                        .align(Alignment.Start)
-                        .padding(start = 6.dp)
-                        .padding(bottom = 6.dp),
+                text = TEAM_JOINED,
+                color = Color.White,
+                modifier = Modifier.padding(vertical = 8.dp),
             )
-
-            Column(
-                modifier =
-                    Modifier
-                        .align(Alignment.Start)
-                        .width(200.dp)
-                        .height(150.dp)
-                        .fillMaxWidth(0.5f)
-                        .padding(start = 6.dp, bottom = 12.dp)
-                        .background(blueGradient, RoundedCornerShape(12.dp))
-                        .padding(12.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceBetween,
-            ) {
-                Text("OPERATIVES", color = Color.White, fontWeight = FontWeight.Bold)
-
-                if (currentRole == PlayerRoles.BLUE_OPERATIVE) {
-                    Text(
-                        text = team_joined,
-                        color = Color.White,
-                        modifier = Modifier.padding(vertical = 8.dp),
-                    )
-                }
-
-                AppButton(
-                    text = join_team,
-                    onClick = { currentRole = PlayerRoles.BLUE_OPERATIVE },
-                    style =
-                        AppButtonStyle(
-                            backgroundBrush = greenGradient,
-                            fontSize = 16.sp,
-                        ),
-                )
-            }
-
-            Column(
-                modifier =
-                    Modifier
-                        .align(Alignment.Start)
-                        .width(200.dp)
-                        .height(150.dp)
-                        .fillMaxWidth(0.5f)
-                        .padding(start = 6.dp, bottom = 12.dp)
-                        .background(blueGradient, RoundedCornerShape(12.dp))
-                        .padding(12.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceBetween,
-            ) {
-                Text("SPYMASTERS", color = Color.White, fontWeight = FontWeight.Bold)
-
-                if (currentRole == PlayerRoles.BLUE_SPYMASTER) {
-                    Text(
-                        text = team_joined,
-                        color = Color.White,
-                        modifier = Modifier.padding(vertical = 8.dp),
-                    )
-                }
-
-                AppButton(
-                    text = join_team,
-                    onClick = { currentRole = PlayerRoles.BLUE_SPYMASTER },
-                    style =
-                        AppButtonStyle(
-                            backgroundBrush = greenGradient,
-                            fontSize = 16.sp,
-                        ),
-                )
-            }
         }
 
-        Column(
-            modifier = Modifier.padding(horizontal = 24.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Column(
-                modifier =
-                    Modifier
-                        .align(Alignment.CenterHorizontally)
-                        .width(400.dp)
-                        .height(250.dp)
-                        .fillMaxWidth(0.5f)
-                        .background(brownGradient, RoundedCornerShape(12.dp))
-                        .padding(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Top,
-            ) {
-                Text(
-                    text = "GAME SETTINGS",
-                    color = Color.White,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.padding(bottom = 16.dp),
-                )
+        AppButton(
+            text = JOIN_TEAM,
+            onClick = { onRoleSelect(role) },
+            style =
+                AppButtonStyle(
+                    backgroundBrush = greenGradient,
+                    fontSize = 16.sp,
+                ),
+        )
+    }
+}
 
-                AppButton(
-                    text = "TIMER: OFF",
-                    onClick = { /* TODO: Timer Logik */ },
-                    modifier =
-                        Modifier
-                            .fillMaxWidth()
-                            .padding(bottom = 8.dp),
-                    style =
-                        AppButtonStyle(
-                            containerColor = Color(0xFF555555),
-                            contentColor = Color.White,
-                            fontSize = 18.sp,
-                        ),
-                )
-            }
+@Composable
+fun GameSettingsColumn(
+    currentRole: PlayerRoles,
+    navController: NavController
+){
+    Column(
+        modifier = Modifier.padding(horizontal = 24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Column(
+            modifier =
+                Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .width(400.dp)
+                    .height(250.dp)
+                    .fillMaxWidth(0.5f)
+                    .background(brownGradient, RoundedCornerShape(12.dp))
+                    .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top,
+        ) {
+            Text(
+                text = "GAME SETTINGS",
+                color = Color.White,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 16.dp),
+            )
 
             AppButton(
-                text = "START GAME",
-                onClick = {
-                    navController.navigate("${Screen.Gameboard.route}/${currentRole.name}")
-                },
+                text = "TIMER: OFF",
+                onClick = { /* TODO: Timer Logik */ },
                 modifier =
                     Modifier
-                        .align(Alignment.CenterHorizontally)
-                        .width(400.dp)
-                        .height(70.dp)
-                        .fillMaxWidth(0.5f)
-                        .padding(top = 12.dp),
+                        .fillMaxWidth()
+                        .padding(bottom = 8.dp),
                 style =
                     AppButtonStyle(
-                        backgroundBrush = greenGradient,
-                        fontSize = 28.sp,
-                        lineHeight = 30.sp,
+                        containerColor = Color(0xFF555555),
+                        contentColor = Color.White,
+                        fontSize = 18.sp,
                     ),
             )
         }
 
-        Column(
-            modifier = Modifier.weight(1f),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Text(
-                text = "RED TEAM",
-                color = Color(0xFFDE8468),
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold,
-                modifier =
-                    Modifier
-                        .align(Alignment.End)
-                        .padding(end = 6.dp)
-                        .padding(bottom = 6.dp),
-            )
-
-            Column(
-                modifier =
-                    Modifier
-                        .align(Alignment.End)
-                        .width(200.dp)
-                        .height(150.dp)
-                        .fillMaxWidth(0.5f)
-                        .padding(end = 6.dp, bottom = 12.dp)
-                        .background(redGradient, RoundedCornerShape(12.dp))
-                        .padding(12.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceBetween,
-            ) {
-                Text("OPERATIVES", color = Color.White, fontWeight = FontWeight.Bold)
-
-                if (currentRole == PlayerRoles.RED_OPERATIVE) {
-                    Text(
-                        text = team_joined,
-                        color = Color.White,
-                        modifier = Modifier.padding(vertical = 8.dp),
-                    )
-                }
-
-                AppButton(
-                    text = join_team,
-                    onClick = { currentRole = PlayerRoles.RED_OPERATIVE },
-                    style =
-                        AppButtonStyle(
-                            backgroundBrush = greenGradient,
-                            fontSize = 16.sp,
-                        ),
-                )
-            }
-
-            Column(
-                modifier =
-                    Modifier
-                        .align(Alignment.End)
-                        .width(200.dp)
-                        .height(150.dp)
-                        .fillMaxWidth(0.5f)
-                        .padding(end = 6.dp, bottom = 12.dp)
-                        .background(redGradient, RoundedCornerShape(12.dp))
-                        .padding(12.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceBetween,
-            ) {
-                Text("SPYMASTERS", color = Color.White, fontWeight = FontWeight.Bold)
-
-                if (currentRole == PlayerRoles.RED_SPYMASTER) {
-                    Text(
-                        text = team_joined,
-                        color = Color.White,
-                        modifier = Modifier.padding(vertical = 8.dp),
-                    )
-                }
-
-                AppButton(
-                    text = join_team,
-                    onClick = { currentRole = PlayerRoles.RED_SPYMASTER },
-                    style =
-                        AppButtonStyle(
-                            backgroundBrush = greenGradient,
-                            fontSize = 16.sp,
-                        ),
-                )
-            }
-        }
+        AppButton(
+            text = "START GAME",
+            onClick = {
+                navController.navigate("${Screen.Gameboard.route}/${currentRole.name}")
+            },
+            modifier =
+                Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .width(400.dp)
+                    .height(70.dp)
+                    .fillMaxWidth(0.5f)
+                    .padding(top = 12.dp),
+            style =
+                AppButtonStyle(
+                    backgroundBrush = greenGradient,
+                    fontSize = 28.sp,
+                    lineHeight = 30.sp,
+                ),
+        )
     }
 }
