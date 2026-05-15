f183376bcae490f6e790df30c5828ad69c2f145a
5eli <prettnerseli@gamil.com>
2026-05-15 18:21:24 +0200

    added ChatViewModelTest

---
+++ app/src/test/java/com/codenames/frontend/viewmodel/ChatViewModelTest.kt
@@ -0,0 +1,82 @@
+package com.codenames.frontend.viewmodel
+
+import com.codenames.frontend.data.model.enums.ChatTab
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+
+class ChatViewModelTest {
+
+    private lateinit var viewModel: ChatViewModel
+
+    @Before
+    fun setup() {
+        viewModel = ChatViewModel()
+    }
+
+    @Test
+    fun updateInput_updatesCurrentInput() {
+        viewModel.updateInput("Hallo")
+
+        assertEquals(
+            "Hallo",
+            viewModel.uiState.value.currentInput
+        )
+    }
+
+    @Test
+    fun sendMessage_addsMessage() {
+        viewModel.updateInput("Neue Nachricht")
+
+        val before =
+            viewModel.uiState.value.messages.size
+
+        viewModel.sendMessage(
+            username = "Max",
+            tab = ChatTab.GLOBAL
+        )
+
+        val state = viewModel.uiState.value
+
+        assertEquals(before + 1, state.messages.size)
+
+        val last = state.messages.last()
+
+        assertEquals("Max", last.sender)
+        assertEquals("Neue Nachricht", last.message)
+        assertEquals(ChatTab.GLOBAL, last.chatTab)
+    }
+
+    @Test
+    fun sendMessage_clearsInputAfterSend() {
+        viewModel.updateInput("Text")
+
+        viewModel.sendMessage(
+            "Max",
+            ChatTab.GLOBAL
+        )
+
+        assertEquals(
+            "",
+            viewModel.uiState.value.currentInput
+        )
+    }
+
+    @Test
+    fun sendMessage_blankMessage_doesNothing() {
+        viewModel.updateInput("     ")
+
+        val before =
+            viewModel.uiState.value.messages.size
+
+        viewModel.sendMessage(
+            "Max",
+            ChatTab.GLOBAL
+        )
+
+        val after =
+            viewModel.uiState.value.messages.size
+
+        assertEquals(before, after)
+    }
+}
