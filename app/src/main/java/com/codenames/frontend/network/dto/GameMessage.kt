f37afa7fe68feedee8fd3127c6cc20dd87292e26
XtophB <xtophb@gmail.com>
2026-05-16 23:26:25 +0200

    feat: add currentPhasae to GameMessage to match backend DTO sent

--- app/src/main/java/com/codenames/frontend/network/dto/GameMessage.kt
+++ app/src/main/java/com/codenames/frontend/network/dto/GameMessage.kt
@@ -1,11 +1,14 @@
 package com.codenames.frontend.network.dto
 
+import com.codenames.frontend.data.model.enums.Role
+import com.codenames.frontend.data.model.enums.Team
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class GameMessage(
-    val winner: String? = null,
-    val currentTurn: String = "",
+    val winner: Team? = null,
+    val currentTurn: Team? = null,
+    val currentPhase: Role? = null,
     val currentRedFound: Int = 0,
     val currentBlueFound: Int = 0,
     val currentClue: String? = null,
