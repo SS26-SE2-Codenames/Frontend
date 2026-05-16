53f3a89010b89260b4af918564dce1d03d624842
XtophB <xtophb@gmail.com>
2026-05-17 00:54:43 +0200

    fix: change attribute to match backend serialization

--- app/src/main/java/com/codenames/frontend/network/dto/ClueDto.kt
+++ app/src/main/java/com/codenames/frontend/network/dto/ClueDto.kt
@@ -3,5 +3,5 @@ package com.codenames.frontend.network.dto
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ClueDto(val word: String, val count: Int) {
+data class ClueDto(val word: String, val guessAmount: Int) {
 }
