52e9ebc7eeffa92476fe8ecbeffbc5dadda3db96
tasaje1 <natasajeftic6@gmail.com>
2026-06-21 18:45:34 +0200

    feat: handle expose cheat results in operative chat

--- app/src/main/java/com/codenames/frontend/data/model/ChatDomainModel.kt
+++ app/src/main/java/com/codenames/frontend/data/model/ChatDomainModel.kt
@@ -1,7 +1,10 @@
 package com.codenames.frontend.data.model
 
+import com.codenames.frontend.data.model.enums.CheatExposureResult
+
 data class ChatDomainModel(
     val sender: String,
     val text: String,
     val isFromMe: Boolean,
+    val cheatExposureResult: CheatExposureResult? = null,
 )
